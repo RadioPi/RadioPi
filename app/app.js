a39921b039fcfda917bd35865eeacb3dc45e536f
Antonio Calapez <antonio.root+radiopi@gmail.com>
2016-02-12 15:34:45 +0100

    Player Test

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 var utils = require('./lib/utils.js');
 var Mplayer = require('node-mplayer');
 
+utils.checkCache();
+
 var player1 = new Mplayer();
 
+player1.setFile('cache/sorry.mp3');
 
+player1.play();
