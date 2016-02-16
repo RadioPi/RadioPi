3c2b1d08af8331c2e3afb415c2a1e60503d7d575
Antonio Calapez <antonio.root+radiopi@gmail.com>
2016-02-16 15:54:32 +0100

    Fix pour la plupart des crashs de node-mplayer

--- app/app.js
+++ app/app.js
@@ -1,10 +1,50 @@
+/**
+* Written by Victor Bury and Antonio Calapez
+**/
+
+/**
+player.setFile() a besoin d'être try/catch
+car il crash si le morceau précédent est fini
+à utiliser de cette manière-ci:
+	try{
+		player.setFile(file);
+	} catch(error) {
+		console.log(error);
+		player = new Mplayer(file);
+	}
+
+player.play() crash si on l'appelle alors qu'un
+morceau est déjà en train d'être joué.
+Il faut donc arrêter le morceau qui est entrain
+d'être joué pour lancer le nouveau
+Utiliser de cette manière-ci:
+	try{
+		player.stop();
+	} catch(error) {
+		console.log(error);
+	}
+	player.play();
+
+player.pause()/player.stop() crash si rien n'est joué,
+pareil que les autres il faut les try/catch:
+	try{
+		player.pause()/stop();
+	} catch(error) {
+		console.log(error);
+	}
+
+player.checkPlaying() retourne:
+	true si le player est en train de jouer ou si il est en pause
+	false dans tous les autres cas.
+**/
 var utils = require('./lib/utils.js');
 var Mplayer = require('node-mplayer');
 
 utils.checkCache();
+var CACHE_PATH = __dirname + "/cache/";
 
-var player1 = new Mplayer();
+var player = new Mplayer();
 
-player1.setFile('cache/sorry.mp3');
+player.setFile(CACHE_PATH + "mlg.mp3");
 
-player1.play();
+player.play();
