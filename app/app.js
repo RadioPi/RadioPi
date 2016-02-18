6cc4e5ce07f70083c3d7cc536012d6f1df7c22bc
Antonio Calapez <antonio.root+radiopi@gmail.com>
2016-02-18 12:18:54 +0100

    Multiple bugs fixés, début de l\'api implémenté

--- app/app.js
+++ app/app.js
@@ -55,22 +55,27 @@ player.checkPlaying() retourne:
 
 var player = new Mplayer();
 
-var queue = ['cache/mlg.mp3', 'cache/xd.mp3', 'cache/triple.mp3'];
+var queue = ['mlg', 'xd', 'triple', 'sorry', 'stitches'];
 var queuePos = 0;
-var AUTO_NEXT = true;
+var AUTO_NEXT = false;
 var REPEAT = true;
 
 var setFile = function(file){
 	try{
-		player.setFile(file);
+		player.setFile(CACHE_PATH + file + '.mp3');
 	} catch (error) {
-		player = new Mplayer(file);
+		player = new Mplayer(CACHE_PATH + file + '.mp3');
 	}
 };
 
+/**
+Appeller stop() après avoir changé un morceau crash le player !
+**/
+
 var play = function(){
-	stop();
+	//stop();
 	player.play();
+	console.log(nowPlaying());
 };
 
 var stop = function(){
@@ -79,8 +84,8 @@ var stop = function(){
 	} catch (error) {
 		console.log(error);
 	}
+	console.log('[nowPlaying]', queue[queuePos]);
 };
-
 
 //TODO: return le state dans lequel le player est
 
@@ -93,7 +98,7 @@ var togglePause = function(){
 };
 
 var next = function(){
-	if(queuePos + 1 > queue.length)
+	if(queuePos + 1 >= queue.length)
 		queuePos = -1;
 	setFile(queue[++queuePos]);
 	play();
@@ -109,9 +114,55 @@ var previous = function(){
 var autoNext = function(){
 	if(!player.checkPlaying()){
 		if(AUTO_NEXT){
-			previous();
+			next();
 		}
 	}
 };
 
 setInterval(autoNext, 1000);
+
+var nowPlaying = function(){
+	return "Now playing: " + queue[queuePos];
+};
+
+/**
+Player Controls API
+**/
+
+var playerRouter = express.Router();
+
+//Sends back all the player routes
+playerRouter.get('/', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({message: 'Hello, world!'}));
+});
+
+playerRouter.get('/togglePause', function(req, res){
+	togglePause();
+	res.send('Calling togglePause() method');
+});
+
+playerRouter.get('/next', function(req, res){
+	next();
+	res.send('Calling next() method');
+});
+
+playerRouter.get('/previous', function(req, res){
+	previous();
+	res.send('Calling previous method');
+});
+
+/**
+Express APP
+**/
+var logger = function(req, res, next){
+	console.log('[logger]', req.method, req.url);
+	next();
+};
+
+app.use(logger);
+app.use('/api/controls', playerRouter);
+
+app.listen(1337, function(){
+	console.log('App listening on port', 1337);
+});
