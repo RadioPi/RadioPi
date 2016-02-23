52e186765cd5c04b1c7b792d25f77e4ad19b30d1
Antonio <antonio.root+radiopi@gmail.com>
2016-02-23 20:18:16 +0100

    Squelette du client web

--- app/app.js
+++ app/app.js
@@ -3,6 +3,29 @@
 * TODO: Fixer l'autonext
 **/
 
+/**
+On utilise webpack-dev-server pour donner l'interface au client
+**/
+
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:3000');
+});
+
+
+//APP
+
 var utils = require('./lib/utils.js');
 var Mplayer = require('node-mplayer');
 var youtube = require('./lib/youtube.js');
