52952fce9dd0879532e152c75ca9bcc3feb66785
Antonio Calapez <antonio.root+radiopi@gmail.com>
2016-05-25 11:34:34 +0200

    Fix de la build Client

--- web_client/client-src/index.js
+++ web_client/client-src/index.js
@@ -1,15 +1,13 @@
-require('./sass/main.scss');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Test from './Test';
 import { Router, Route, browserHistory } from 'react-router';
+require('./sass/main.scss');
+
 
 
 ReactDOM.render(
 	<Router history={browserHistory}>
 		<Route path="/" component={App}/>
-		<Route path="/test" component={Test}/>
 	</Router>
 	, document.getElementById('root'));
